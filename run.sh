e898498dcdd55a489e25229bfc8c4bfe83d6cf4d
--global <--global>
2025-07-23 14:50:15 -0700

    draft of automatic setup for git prompt and vim prompt

--- run.sh
+++ run.sh
@@ -7,3 +7,14 @@ ln -s $cur/.pylintrc ~/.pylintrc
 ln -s $cur/.tmux.conf ~/.tmux.conf
 ln -s $cur/.vimrc ~/.vimrc
 ln -s $cur/.bash_vars ~/.bash_vars
+ln -s $cur/.setup ~/.setup
+
+echo 'if [[ -f "$HOME/.bash_aliases" ]]; then
+  source "$HOME/.bash_aliases"
+fi' >> ~/.bashrc
+echo 'if [[ -f "$HOME/.functions.sh" ]]; then
+  source "$HOME/.functions.sh"
+fi' >> ~/.bashrc
+echo 'if [[ -f "$HOME/.setup" ]]; then
+  source "$HOME/.setup"
+fi' >> ~/.bashrc
