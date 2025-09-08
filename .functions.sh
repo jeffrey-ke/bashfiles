7ce312f74ef0d9f2576c76a088587becc6c5764e
Jeffrey Ke <jeffke613@gmail.com>
2025-09-08 11:25:32 -0400

    machine specific

--- .functions.sh
+++ .functions.sh
@@ -324,3 +324,16 @@ gshare() {
     echo "Link: $link"
     echo "(logged to $_gdrive_share_log)"
 }
+gcvsub(){
+    if [[ $# -lt 2 ]]; then
+        echo "Provide proj dir with trailing / and the number of the assignment as the first 2 arguments"
+        return 1
+    fi
+    proj_dir=$1
+    if [[ $proj_dir != */ ]]; then
+        echo "Add leading slash to first argument"
+        return 1
+    fi
+    proj_num=$2
+    rsync --partial -av --exclude .git $proj_dir jke3@linux.andrew.cmu.edu:/afs/andrew.cmu.edu/course/16/822/www/projects/jke3/proj$proj_num
+}
