ac7515ce025f69083647d784e119620f1c90be1b
jeff <jke3@cs.cmu.edu>
2025-12-13 16:06:39 -0500

    sanitizing run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ $1 = "bash" ]]; then
-    filerc=".bashrc"
+	filerc=".bashrc"
 elif [[ $1 = "zsh" ]]; then
-    filerc=".zshrc"
+	filerc=".zshrc"
 else
-    echo "First arg to setup script must be either zsh or bash"
-    exit 1
+	echo "First arg to setup script must be either zsh or bash"
+	exit 1
 fi
 cur=~/dotfiles
 ln -s $cur/.bash_aliases ~/.bash_aliases
@@ -14,24 +14,25 @@ ln -s $cur/.functions.sh ~/.functions.sh
 ln -s $cur/.pylintrc ~/.pylintrc
 ln -s $cur/.tmux.conf ~/.tmux.conf
 ln -s $cur/.vimrc ~/.vimrc
-ln -s $cur/.bash_vars ~/.bash_vars
-ln -s $cur/.setup ~/.setup
+# ln -s $cur/.bash_vars ~/.bash_vars
+ln -s $cur/pyrightconfig.json ~/.config/pyright/pyrightconfig.json
+# ln -s $cur/.setup ~/.setup
 
 # Add source lines only if they don't already exist
 if ! grep -q 'source "$HOME/.bash_aliases"' ~/$filerc; then
-    echo 'if [[ -f "$HOME/.bash_aliases" ]]; then
+	echo 'if [[ -f "$HOME/.bash_aliases" ]]; then
   source "$HOME/.bash_aliases"
-fi' >> ~/$filerc
+fi' >>~/$filerc
 fi
 
 if ! grep -q 'source "$HOME/.functions.sh"' ~/$filerc; then
-    echo 'if [[ -f "$HOME/.functions.sh" ]]; then
+	echo 'if [[ -f "$HOME/.functions.sh" ]]; then
   source "$HOME/.functions.sh"
-fi' >> ~/$filerc
+fi' >>~/$filerc
 fi
 
-if ! grep -q 'source "$HOME/.setup"' ~/$filerc; then
-    echo 'if [[ -f "$HOME/.setup" ]]; then
-  source "$HOME/.setup"
-fi' >> ~/$filerc
-fi
+# if ! grep -q 'source "$HOME/.setup"' ~/$filerc; then
+# 	echo 'if [[ -f "$HOME/.setup" ]]; then
+#   source "$HOME/.setup"
+# fi' >>~/$filerc
+# fi
