107994034d3f6401c05c97de74315f8503599213
Jeffrey Ke <jeffke613@gmail.com>
2026-04-14 18:25:04 -0400

    made the cd function use zoxide instead

--- machines/tesu.sh
+++ machines/tesu.sh
@@ -10,25 +10,25 @@ export PATH="$HOME/.local/bin:$PATH"
 export PATH=$HOME/nvim-linux-x86_64/bin:$PATH
 export PATH=$HOME/isaacsim:$PATH
 
-alias z=cd
-function cd () {
-  __zoxide_z "$@" || return $?
-  [[ -n "$_CD_SOURCING" ]] && return 0
-  local _CD_SOURCING=1
-  local accumulator="" remaining="${PWD#/}"
-  while [[ -n "$remaining" ]]; do
-    if [[ "$remaining" == */* ]]; then
-      accumulator+="/${remaining%%/*}"
-      remaining="${remaining#*/}"
-    else
-      accumulator+="/$remaining"
-      remaining=""
-    fi
-    if [[ -f "$accumulator/.aliases" ]]; then
-      echo "Sourcing $accumulator/.aliases:"
-      cat "$accumulator/.aliases"
-      source "$accumulator/.aliases"
-    fi
-  done
-  return 0
+function cd() {
+	__zoxide_z "$@" || return $?
+	[[ -n "$_CD_SOURCING" ]] && return 0
+	local _CD_SOURCING=1
+	local accumulator="" remaining="${PWD#/}"
+	while [[ -n "$remaining" ]]; do
+		if [[ "$remaining" == */* ]]; then
+			accumulator+="/${remaining%%/*}"
+			remaining="${remaining#*/}"
+		else
+			accumulator+="/$remaining"
+			remaining=""
+		fi
+		if [[ -f "$accumulator/.aliases" ]]; then
+			echo "Sourcing $accumulator/.aliases:"
+			cat "$accumulator/.aliases"
+			source "$accumulator/.aliases"
+		fi
+	done
+	return 0
 }
+alias z=cd
